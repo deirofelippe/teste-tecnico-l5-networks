932f1d629441b771e65952600b6369997c144d7f
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-09 21:49:14 -0300

    test(integration): cria teste para FindAllFilms

---
+++ tests/integration/FindAllFilmsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__."/../../src/HttpClient.php";
+require_once __DIR__."/../../src/Cache.php";
+require_once __DIR__."/../../src/FindAllFilms.php";
+
+use PHPUnit\Framework\TestCase;
+
+final class FindAllFilmsTest extends TestCase
+{
+    protected function tearDown(): void
+    {
+        \Mockery::close();
+    }
+
+    public function test_deve_buscar_filmes(): void
+    {
+        $mock_cache = \Mockery::mock(Cache::class);
+        $mock_cache->shouldReceive('get')->with("find_all_films")->andReturn([]);
+        $mock_cache->shouldReceive('set')->andReturn();
+
+        $mock_http_client = \Mockery::mock(HttpClient::class);
+        $mock_response = ["results" => [["url" => "https://teste.com/125/","title" => "titulo teste","release_date" => "1977-10-01"]]];
+        $mock_http_client->shouldReceive('get')->andReturn($mock_response);
+
+        $service = new FindAllFilms($mock_http_client, $mock_cache);
+
+        $films = $service->execute();
+
+        $this->assertSame(1, count($films));
+
+        $this->assertSame("125", $films[0]["id"]);
+        $this->assertSame("titulo teste", $films[0]["title"]);
+        $this->assertSame("1977-10-01", $films[0]["release_date"]);
+    }
+}
