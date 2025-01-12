d487d0d28a37dc933ade5888ffcf367d071f22a7
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-12 14:15:19 -0300

    feat: adiciona logger e tratamento de erros no HttpClient

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -2,18 +2,43 @@
 
 class HttpClient
 {
+    private Logger $logger;
+
+    public function __construct(
+        Logger $logger,
+    ) {
+        $this->logger = $logger;
+    }
+
     public function get(string $url): array
     {
+        $execute = function () use ($url): array {
+            $this->logger->register("INFO", "HTTP_CLIENT", "Fazendo requisição");
+            $this->logger->register("DEBUG", "HTTP_CLIENT", "METHOD: GET");
+            $this->logger->register("DEBUG", "HTTP_CLIENT", "URL: $url");
 
-        $ch = curl_init();
+            $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        curl_close($ch);
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $output = curl_exec($ch);
+            curl_close($ch);
 
-        $output_json = json_decode($output, true);
-        return $output_json;
+            $this->logger->register("DEBUG", "HTTP_CLIENT", "Response: \n\n".$output);
+
+            $output_json = json_decode($output, true);
+            return $output_json;
+        };
+
+        try {
+            return $execute();
+        } catch (\Exception $e) {
+            $this->logger->register("ERROR", "HTTP_CLIENT", "Erro ao fazer requisição");
+            $this->logger->register("ERROR", "HTTP_CLIENT", "Message: \n\n".$e->getMessage());
+            $this->logger->register("ERROR", "HTTP_CLIENT", "Trace: \n\n".json_encode($e->getTrace()));
+
+            throw $e;
+        }
     }
 
     public function post()
