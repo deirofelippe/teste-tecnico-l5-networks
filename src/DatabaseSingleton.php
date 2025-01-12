27b9008ae51ed2a8f11684c749f546e2d05eef0e
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-12 16:56:07 -0300

    config(database): adiciona port, options e melhora codigo em DatabaseSingleton

--- src/DatabaseSingleton.php
+++ src/DatabaseSingleton.php
@@ -15,13 +15,20 @@ class DatabaseSingleton
         $instance = self::$instance;
         if (self::$instance === null) {
             $host = DB_HOST;
+            $port = DB_PORT;
             $database = DB_DATABASE;
             $user = DB_USER;
             $password = DB_PASSWORD;
+            $charset = "utf8mb4";
 
-            $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password, [
-                PDO::ATTR_PERSISTENT => true
-            ]);
+            $options = [
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            ];
+
+            $dsn = "mysql:host=$host;port=$port;dbname=$database;charset=$charset";
+            $pdo = new PDO($dsn, $user, $password, $options);
 
             self::$instance = $pdo;
         }
