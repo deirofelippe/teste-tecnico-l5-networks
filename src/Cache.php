01c49a7894ac3208d2b6d7a50453b13515a78db6
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-12 14:15:57 -0300

    feat: adiciona logger e tratamento de erros no Cache

--- src/Cache.php
+++ src/Cache.php
@@ -2,25 +2,66 @@
 
 class Cache
 {
+    private Logger $logger;
+
+    public function __construct(
+        Logger $logger,
+    ) {
+        $this->logger = $logger;
+    }
+
     public function get(string $name): array
     {
-        $file = __DIR__."/../cache/$name.json";
+        $execute = function () use ($name): array {
+            $file = __DIR__."/../cache/$name.json";
 
-        $not_exists = !file_exists($file);
-        if ($not_exists) {
+
+            $not_exists = !file_exists($file);
+            if ($not_exists) {
+                return [];
+            }
+
+            $this->logger->register("INFO", "CACHE", "Busca de dados no cache, cache_name '$name'");
+            $data = file_get_contents($file);
+
+            $this->logger->register("DEBUG", "CACHE", "Busca feita");
+            $this->logger->register("DEBUG", "CACHE", "Dados: \n\n$data");
+            $data = json_decode($data, true);
+
+            return $data;
+        };
+
+        try {
+            return $execute();
+        } catch (\Exception $e) {
+            $this->logger->register("ERROR", "CACHE", "Erro ao buscar dados no cache");
+            $this->logger->register("ERROR", "CACHE", "Input: \n\nName: $name");
+            $this->logger->register("ERROR", "CACHE", "Message: \n\n".$e->getMessage());
+            $this->logger->register("ERROR", "CACHE", "Trace: \n\n".json_encode($e->getTrace()));
+
             return [];
         }
-
-        $data = file_get_contents($file);
-        $data = json_decode($data, true);
-
-        return $data;
     }
 
     public function set(string $name, array $data): void
     {
-        $data = json_encode($data);
-        $file = __DIR__."/../cache/$name.json";
-        file_put_contents($file, $data);
+        $execute = function () use ($name, $data) {
+            $this->logger->register("INFO", "CACHE", "Inserção de dados no cache, cache_name '$name'");
+
+            $data = json_encode($data);
+            $this->logger->register("DEBUG", "CACHE", "Dados: \n\n $data");
+
+            $file = __DIR__."/../cache/$name.json";
+            file_put_contents($file, $data);
+        };
+
+        try {
+            $execute();
+        } catch (\Exception $e) {
+            $this->logger->register("ERROR", "CACHE", "Erro ao adicionar dados no cache");
+            $this->logger->register("ERROR", "CACHE", "Input: \n\nName: $name\nData: \n\n".json_encode($data));
+            $this->logger->register("ERROR", "CACHE", "Message: \n\n".$e->getMessage());
+            $this->logger->register("ERROR", "CACHE", "Trace: \n\n".json_encode($e->getTrace()));
+        }
     }
 }
