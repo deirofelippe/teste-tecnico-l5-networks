959c44f92332e043ccc42bfa8d9657c258f907df
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-18 18:50:01 -0300

    tests(e2e): atualiza as dependencias de FindFilmByIdTest

--- tests/e2e/FindFilmByIdTest.php
+++ tests/e2e/FindFilmByIdTest.php
@@ -1,11 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
-require_once __DIR__."/../../src/HttpClient.php";
-require_once __DIR__."/../../src/Logger.php";
-require_once __DIR__."/../../src/Cache.php";
-require_once __DIR__."/../../src/FindFilmById.php";
+require_once __DIR__."/../../src/utils/RequireAll.php";
 
 use PHPUnit\Framework\TestCase;
 
@@ -13,9 +8,8 @@ final class FindFilmByIdTest extends TestCase
 {
     public function test_deve_buscar_filme_pelo_id(): void
     {
-        date_default_timezone_set("America/Sao_Paulo");
-
-        $logger = new Logger();
+        $pdo = DatabaseSingleton::getInstance();
+        $logger = new Logger(new LogsRepository($pdo));
         $httpClient = new HttpClient($logger);
         $cache = new Cache($logger);
 
