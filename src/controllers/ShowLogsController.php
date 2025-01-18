dde1130421f015eced5c9ca149a55a12d0811687
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-18 18:40:37 -0300

    feat(controllers): cria ShowLogsController

---
+++ src/controllers/ShowLogsController.php
@@ -0,0 +1,22 @@
+<?php
+
+class ShowLogsController extends View
+{
+    private ShowLogsService $service;
+
+    public function __construct(
+        ShowLogsService $service,
+    ) {
+        $this->service = $service;
+    }
+
+    public function execute(string $limit, string $offset)
+    {
+        $limit = intval($limit);
+        $offset = intval($offset);
+
+        $response = $this->service->execute($limit, $offset);
+
+        $this->render_page($response, "logs");
+    }
+}
