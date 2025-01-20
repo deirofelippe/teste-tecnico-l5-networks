efb8c5a0d9d722b2fdada8f89aafbaf34a0ae38b
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-19 22:46:25 -0300

    scripts: atualiza criacao de banco de dados com novas tabelas

--- database.sql
+++ database.sql
@@ -9,3 +9,23 @@ CREATE TABLE IF NOT EXISTS Log (
     `context` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL, 
     `description` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
 ) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci; 
+
+CREATE TABLE IF NOT EXISTS Film (
+    `id` INT PRIMARY KEY NOT NULL,
+    `name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci; 
+
+CREATE TABLE IF NOT EXISTS Author (
+    `id` INT PRIMARY KEY NOT NULL, 
+    `name` VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci; 
+
+CREATE TABLE IF NOT EXISTS Comment (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+    `film_id` INT NOT NULL, 
+    `author_id` INT NOT NULL, 
+    `comment` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL, 
+    `created_at` DATETIME NOT NULL,
+    FOREIGN KEY (`film_id`) REFERENCES Film(`id`),
+    FOREIGN KEY (`author_id`) REFERENCES Author(`id`)
+) ENGINE = InnoDB CHARSET=utf8mb4 COLLATE utf8mb4_0900_ai_ci; 
