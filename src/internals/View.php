0e113ca8d67f630d2ffdb80d086951101cbafe1d
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-20 15:27:09 -0300

    feat(internals): cria funcao para terminar request e retornar json, adiciona url do backend no render_page em View

--- src/internals/View.php
+++ src/internals/View.php
@@ -4,6 +4,22 @@ class View
 {
     protected function render_page(array $data, string $page_name)
     {
+        $backend_url = 'http://localhost:3000';
+
         require_once __DIR__ . '/../views/index.php';
+    }
+
+    protected function finish_request(int $status_code, array $response_data)
+    {
+        http_response_code($status_code);
+
+        if (count($response_data) < 1) {
+            exit(0);
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode($response_data);
+
+        exit(0);
     }
 }
