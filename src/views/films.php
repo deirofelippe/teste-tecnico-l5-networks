48b30ab2443a690e53579dc165768c33e4c6fa31
Felippe Deiró <sergiofelippe.deiro@gmail.com>
2025-01-20 15:20:29 -0300

    feat(views): adiciona secao de comentarios no /film/{id}

--- src/views/films.php
+++ src/views/films.php
@@ -1,15 +1,15 @@
 <div class="container">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 text-center">
-        <?php foreach ($data as $index => $film) { ?>
-          <div class="col gy-4 ">
-            <div class="card">
-              <div class="card-body">
-                <h1 class="card-title h-50"><?=$film['title'] ?></h1>
-                <p class="card-subtitle h-25 d-flex flex-center"><?=$film['release_date'] ?></p>
-                <a class="btn btn-secondary h-25 w-100" href="/film/<?=$film['id'] ?>">Ver detalhes</a>
-              </div>
-            </div>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 text-center">
+    <?php foreach ($data as $index => $film) { ?>
+      <div class="col gy-4 ">
+        <div class="card">
+          <div class="card-body">
+            <h1 class="card-title h-50"><?=$film['title'] ?></h1>
+            <p class="card-subtitle h-25 d-flex flex-center"><?=$film['release_date'] ?></p>
+            <a class="btn btn-secondary h-25 w-100" href="/film/<?=$film['id'] ?>">Ver detalhes</a>
           </div>
-        <?php } ?>
+        </div>
       </div>
+    <?php } ?>
+  </div>
 </div>
